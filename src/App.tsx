86d8f9c806a000bc755549c0f4bb9e5867faefd6
autumnly1007 <autumnly1007@gmail.com>
2023-01-12 13:43:52 +0900

    Add redux middleware

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { RootState } from './reducers';
 import './App.css';
 
 type Props = {
-  value: number;
   onIncrement: () => void;
   onDecrement: () => void;
 };
 
-function App({ value, onIncrement, onDecrement }: Props) {
+function App({ onIncrement, onDecrement }: Props) {
+  const [todoValue, setTodoValue] = useState('');
+  const counter = useSelector((state: RootState) => state.counter);
+  const todos: string[] = useSelector((state: RootState) => state.todos);
+  const dispatch = useDispatch();
+
+  const addTodo = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    dispatch({ type: 'ADD_TODO', text: todoValue });
+    setTodoValue('');
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTodoValue(e.target.value);
+  };
+
   return (
-    <p>
-      Clicked: {value} times <button onClick={onIncrement}>+</button>{' '}
-      <button onClick={onDecrement}>-</button>
-    </p>
+    <div className='App'>
+      <p>Clicked: {counter} times</p>
+      <button onClick={() => dispatch({ type: 'INCREMENT' })}>+</button>
+      <button onClick={() => dispatch({ type: 'DECREMENT' })}>-</button>
+
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>{todo}</li>
+        ))}
+      </ul>
+
+      <form onSubmit={addTodo}>
+        <input type='text' value={todoValue} onChange={handleChange} />
+        <input type='submit' />
+      </form>
+    </div>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,22 +3,37 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { createStore } from 'redux';
-import counter from './reducers';
+import { applyMiddleware, createStore } from 'redux';
+import rootReducer from './reducers';
+import { Provider } from 'react-redux';
+import loggerMiddleware from './middleware/log';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
-const store = createStore(counter);
+const middleware = applyMiddleware(loggerMiddleware);
 
-root.render(
-  <React.StrictMode>
-    <App
-      value={store.getState()}
-      onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
-      onDecrement={() => store.dispatch({ type: 'DECREMENT' })}
-    />
-  </React.StrictMode>
-);
+const store = createStore(rootReducer, middleware);
+
+store.dispatch({
+  type: 'ADD_TODO',
+  text: '밥먹기',
+});
+console.log(store.getState());
+
+const render = () =>
+  root.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <App
+          onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
+          onDecrement={() => store.dispatch({ type: 'DECREMENT' })}
+        />
+      </Provider>
+    </React.StrictMode>
+  );
+render();
+
+store.subscribe(render);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
